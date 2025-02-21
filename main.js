4c7cb29f59df58ece8ae430e10aebed8b0ee5f80
Syou Zhang <azwsxrfv2396187@gmail.com>
2025-02-21 09:53:00 +0900

    update

---
+++ main.js
@@ -0,0 +1,336 @@
+//清單
+const fullList = [
+    { "name": "あ", "team": ["seionH"] },
+    { "name": "い", "team": ["seionH"] },
+    { "name": "う", "team": ["seionH"] },
+    { "name": "え", "team": ["seionH"] },
+    { "name": "お", "team": ["seionH"] },
+    { "name": "か", "team": ["seionH"] },
+    { "name": "き", "team": ["seionH"] },
+    { "name": "く", "team": ["seionH"] },
+    { "name": "け", "team": ["seionH"] },
+    { "name": "こ", "team": ["seionH"] },
+    { "name": "さ", "team": ["seionH"] },
+    { "name": "し", "team": ["seionH"] },
+    { "name": "す", "team": ["seionH"] },
+    { "name": "せ", "team": ["seionH"] },
+    { "name": "そ", "team": ["seionH"] },
+    { "name": "た", "team": ["seionH"] },
+    { "name": "ち", "team": ["seionH"] },
+    { "name": "つ", "team": ["seionH"] },
+    { "name": "て", "team": ["seionH"] },
+    { "name": "と", "team": ["seionH"] },
+    { "name": "な", "team": ["seionH"] },
+    { "name": "に", "team": ["seionH"] },
+    { "name": "ぬ", "team": ["seionH"] },
+    { "name": "ね", "team": ["seionH"] },
+    { "name": "の", "team": ["seionH"] },
+    { "name": "は", "team": ["seionH"] },
+    { "name": "ひ", "team": ["seionH"] },
+    { "name": "ふ", "team": ["seionH"] },
+    { "name": "へ", "team": ["seionH"] },
+    { "name": "ほ", "team": ["seionH"] },
+    { "name": "ま", "team": ["seionH"] },
+    { "name": "み", "team": ["seionH"] },
+    { "name": "む", "team": ["seionH"] },
+    { "name": "め", "team": ["seionH"] },
+    { "name": "も", "team": ["seionH"] },
+    { "name": "や", "team": ["seionH"] },
+    { "name": "ゆ", "team": ["seionH"] },
+    { "name": "よ", "team": ["seionH"] },
+    { "name": "ら", "team": ["seionH"] },
+    { "name": "り", "team": ["seionH"] },
+    { "name": "る", "team": ["seionH"] },
+    { "name": "れ", "team": ["seionH"] },
+    { "name": "ろ", "team": ["seionH"] },
+    { "name": "わ", "team": ["seionH"] },
+    { "name": "を", "team": ["seionH"] },
+    { "name": "ん", "team": ["bionH"] },
+    { "name": "ア", "team": ["seionK"] },
+    { "name": "イ", "team": ["seionK"] },
+    { "name": "ウ", "team": ["seionK"] },
+    { "name": "エ", "team": ["seionK"] },
+    { "name": "オ", "team": ["seionK"] },
+    { "name": "カ", "team": ["seionK"] },
+    { "name": "キ", "team": ["seionK"] },
+    { "name": "ク", "team": ["seionK"] },
+    { "name": "ケ", "team": ["seionK"] },
+    { "name": "コ", "team": ["seionK"] },
+    { "name": "サ", "team": ["seionK"] },
+    { "name": "シ", "team": ["seionK"] },
+    { "name": "ス", "team": ["seionK"] },
+    { "name": "セ", "team": ["seionK"] },
+    { "name": "ソ", "team": ["seionK"] },
+    { "name": "タ", "team": ["seionK"] },
+    { "name": "チ", "team": ["seionK"] },
+    { "name": "ツ", "team": ["seionK"] },
+    { "name": "テ", "team": ["seionK"] },
+    { "name": "ト", "team": ["seionK"] },
+    { "name": "ナ", "team": ["seionK"] },
+    { "name": "ニ", "team": ["seionK"] },
+    { "name": "ヌ", "team": ["seionK"] },
+    { "name": "ネ", "team": ["seionK"] },
+    { "name": "ノ", "team": ["seionK"] },
+    { "name": "ハ", "team": ["seionK"] },
+    { "name": "ヒ", "team": ["seionK"] },
+    { "name": "フ", "team": ["seionK"] },
+    { "name": "ヘ", "team": ["seionK"] },
+    { "name": "ホ", "team": ["seionK"] },
+    { "name": "マ", "team": ["seionK"] },
+    { "name": "ミ", "team": ["seionK"] },
+    { "name": "ム", "team": ["seionK"] },
+    { "name": "メ", "team": ["seionK"] },
+    { "name": "モ", "team": ["seionK"] },
+    { "name": "ヤ", "team": ["seionK"] },
+    { "name": "ユ", "team": ["seionK"] },
+    { "name": "ヨ", "team": ["seionK"] },
+    { "name": "ラ", "team": ["seionK"] },
+    { "name": "リ", "team": ["seionK"] },
+    { "name": "ル", "team": ["seionK"] },
+    { "name": "レ", "team": ["seionK"] },
+    { "name": "ロ", "team": ["seionK"] },
+    { "name": "ワ", "team": ["seionK"] },
+    { "name": "ヲ", "team": ["seionK"] },
+    { "name": "ン", "team": ["bionK"] },
+    { "name": "が", "team": ["dakuonH"] },
+    { "name": "ぎ", "team": ["dakuonH"] },
+    { "name": "ぐ", "team": ["dakuonH"] },
+    { "name": "げ", "team": ["dakuonH"] },
+    { "name": "ご", "team": ["dakuonH"] },
+    { "name": "ざ", "team": ["dakuonH"] },
+    { "name": "じ", "team": ["dakuonH"] },
+    { "name": "ず", "team": ["dakuonH"] },
+    { "name": "ぜ", "team": ["dakuonH"] },
+    { "name": "ぞ", "team": ["dakuonH"] },
+    { "name": "だ", "team": ["dakuonH"] },
+    { "name": "ぢ", "team": ["dakuonH"] },
+    { "name": "づ", "team": ["dakuonH"] },
+    { "name": "で", "team": ["dakuonH"] },
+    { "name": "ど", "team": ["dakuonH"] },
+    { "name": "ば", "team": ["dakuonH"] },
+    { "name": "び", "team": ["dakuonH"] },
+    { "name": "ぶ", "team": ["dakuonH"] },
+    { "name": "べ", "team": ["dakuonH"] },
+    { "name": "ぼ", "team": ["dakuonH"] },
+    { "name": "ガ", "team": ["dakuonK"] },
+    { "name": "ギ", "team": ["dakuonK"] },
+    { "name": "グ", "team": ["dakuonK"] },
+    { "name": "ゲ", "team": ["dakuonK"] },
+    { "name": "ゴ", "team": ["dakuonK"] },
+    { "name": "ザ", "team": ["dakuonK"] },
+    { "name": "ジ", "team": ["dakuonK"] },
+    { "name": "ズ", "team": ["dakuonK"] },
+    { "name": "ゼ", "team": ["dakuonK"] },
+    { "name": "ゾ", "team": ["dakuonK"] },
+    { "name": "ダ", "team": ["dakuonK"] },
+    { "name": "ヂ", "team": ["dakuonK"] },
+    { "name": "ヅ", "team": ["dakuonK"] },
+    { "name": "デ", "team": ["dakuonK"] },
+    { "name": "ド", "team": ["dakuonK"] },
+    { "name": "バ", "team": ["dakuonK"] },
+    { "name": "ビ", "team": ["dakuonK"] },
+    { "name": "ブ", "team": ["dakuonK"] },
+    { "name": "ベ", "team": ["dakuonK"] },
+    { "name": "ボ", "team": ["dakuonK"] },
+    { "name": "ぱ", "team": ["handakuonH"] },
+    { "name": "ぴ", "team": ["handakuonH"] },
+    { "name": "ぷ", "team": ["handakuonH"] },
+    { "name": "ぺ", "team": ["handakuonH"] },
+    { "name": "ぽ", "team": ["handakuonH"] },
+    { "name": "パ", "team": ["handakuonK"] },
+    { "name": "ピ", "team": ["handakuonK"] },
+    { "name": "プ", "team": ["handakuonK"] },
+    { "name": "ペ", "team": ["handakuonK"] },
+    { "name": "ポ", "team": ["handakuonK"] },
+    { "name": "きゃ", "team": ["youonH"] },
+    { "name": "きゅ", "team": ["youonH"] },
+    { "name": "きょ", "team": ["youonH"] },
+    { "name": "しゃ", "team": ["youonH"] },
+    { "name": "しゅ", "team": ["youonH"] },
+    { "name": "しょ", "team": ["youonH"] },
+    { "name": "ちゃ", "team": ["youonH"] },
+    { "name": "ちゅ", "team": ["youonH"] },
+    { "name": "ちょ", "team": ["youonH"] },
+    { "name": "にゃ", "team": ["youonH"] },
+    { "name": "にゅ", "team": ["youonH"] },
+    { "name": "にょ", "team": ["youonH"] },
+    { "name": "ひゃ", "team": ["youonH"] },
+    { "name": "ひゅ", "team": ["youonH"] },
+    { "name": "ひょ", "team": ["youonH"] },
+    { "name": "みゃ", "team": ["youonH"] },
+    { "name": "みゅ", "team": ["youonH"] },
+    { "name": "みょ", "team": ["youonH"] },
+    { "name": "りゃ", "team": ["youonH"] },
+    { "name": "りゅ", "team": ["youonH"] },
+    { "name": "りょ", "team": ["youonH"] },
+    { "name": "キャ", "team": ["youonK"] },
+    { "name": "キュ", "team": ["youonK"] },
+    { "name": "キョ", "team": ["youonK"] },
+    { "name": "シャ", "team": ["youonK"] },
+    { "name": "シュ", "team": ["youonK"] },
+    { "name": "ショ", "team": ["youonK"] },
+    { "name": "チャ", "team": ["youonK"] },
+    { "name": "チュ", "team": ["youonK"] },
+    { "name": "チョ", "team": ["youonK"] },
+    { "name": "ニャ", "team": ["youonK"] },
+    { "name": "ニュ", "team": ["youonK"] },
+    { "name": "ニョ", "team": ["youonK"] },
+    { "name": "ヒャ", "team": ["youonK"] },
+    { "name": "ヒュ", "team": ["youonK"] },
+    { "name": "ヒョ", "team": ["youonK"] },
+    { "name": "ミャ", "team": ["youonK"] },
+    { "name": "ミュ", "team": ["youonK"] },
+    { "name": "ミョ", "team": ["youonK"] },
+    { "name": "リャ", "team": ["youonK"] },
+    { "name": "リュ", "team": ["youonK"] },
+    { "name": "リョ", "team": ["youonK"] }
+
+];
+
+//取得元素
+//input的元素
+const seionHEl = document.getElementById("seionH");
+const seionKEl = document.getElementById("seionK");
+const bionHEl = document.getElementById("bionH");
+const bionKEl = document.getElementById("bionK");
+const dakuonHEl = document.getElementById("dakuonH");
+const dakuonKEl = document.getElementById("dakuonK");
+const handakuonHEl = document.getElementById("handakuonH");
+const handakuonKEl = document.getElementById("handakuonK");
+const youonHEl = document.getElementById("youonH");
+const youonKEl = document.getElementById("youonK");
+
+
+//按鈕元素
+const btnEl = document.querySelector("button");
+//名字元素
+const nameEl = document.getElementById("name");
+
+
+
+
+//抽選按鈕
+btnEl.addEventListener("click", function () {
+
+    //如果全部沒勾
+    if (seionHEl.checked === false &&
+        seionKEl.checked === false &&
+        bionHEl.checked === false &&
+        bionKEl.checked === false &&
+        dakuonHEl.checked === false &&
+        dakuonKEl.checked === false &&
+        handakuonHEl.checked === false &&
+        handakuonKEl.checked === false &&
+        youonHEl.checked === false &&
+        youonKEl.checked === false) {
+        // console.log("進入沒勾的方法");
+        let randomNum = Math.floor(Math.random() * 200)
+        while (randomNum >= fullList.length) {
+            randomNum = Math.floor(Math.random() * 200)
+        }
+        const result = fullList[randomNum].name;
+        nameEl.innerText = result;
+    } else {
+        //建立空清單
+        let checkListed = [];
+        // console.log("有勾");
+        //清音(平假名)
+        if (seionHEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("seionH") !== -1) {
+                    checkListed.push(item.name);
+                    console.log(checkListed);
+
+
+                }
+            })
+        }
+        //清音(片假名)
+        if (seionKEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("seionK") !== -1) {
+                    checkListed.push(item.name);
+                    console.log(checkListed);
+
+
+                }
+            })
+        }
+        //鼻音(平假名)有勾
+        if (bionHEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("bionH") !== -1) {
+                    checkListed.push(item.name);
+                }
+            })
+        }
+        //鼻音(片假名)有勾
+        if (bionKEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("bionK") !== -1) {
+                    checkListed.push(item.name);
+                }
+            })
+        }
+        //濁音(平假名)有勾
+        if (dakuonHEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("dakuonH") !== -1) {
+                    checkListed.push(item.name);
+                }
+            })
+        }
+        //濁音(片假名)有勾
+        if (dakuonKEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("dakuonK") !== -1) {
+                    checkListed.push(item.name);
+                }
+            })
+        }
+        //半濁音(平假名)有勾
+        if (handakuonHEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("handakuonH") !== -1) {
+                    checkListed.push(item.name);
+                }
+            })
+        }
+        //半濁音(片假名)有勾
+        if (handakuonKEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("handakuonK") !== -1) {
+                    checkListed.push(item.name);
+                }
+            })
+        }
+        //拗音(平假名)有勾
+        if (youonHEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("youonH") !== -1) {
+                    checkListed.push(item.name);
+                }
+            })
+        }
+        //拗音(片假名)有勾
+        if (youonKEl.checked === true) {
+            fullList.forEach((item) => {
+                if (item.team.indexOf("youonK") !== -1) {
+                    checkListed.push(item.name);
+                }
+            })
+        }
+        //從有打勾的人裡面選一位出來
+        let randomNum = Math.floor(Math.random() * 100)
+        while (randomNum >= checkListed.length) {
+            randomNum = Math.floor(Math.random() * 100)
+        }
+        result = checkListed[randomNum];
+        nameEl.innerText = result;
+    }
+
+
+
+})
+
+
